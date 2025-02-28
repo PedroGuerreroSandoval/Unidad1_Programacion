5854c4fc0b5d34b4bc9f5d70b3cd1212353f5974
PedroGuerreroSandoval <pedroguerrerosandoval7805@gmail.com>
2025-02-27 20:03:46 -0600

    Create Ejemplo 1.6.cpp

---
+++ Practicas Cpp/Ejemplo 1.6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, D;
+    
+    cout << "Ingresa los datos que quiera invertir " << "\n";
+    cout << "Porfavor ingrese el valor de A" << "\n";
+    cin >> A;
+    cout << "Porfavor ingrese el valor de B" << "\n";
+    cin >> B;
+    cout << "Porfavor ingrese el valor de C" << "\n";
+    cin >> C;
+    cout << "Porfavor ingrese el valor de D" << "\n";
+    cin >> D;
+    cout << "Los numeros que ingreso pero en orden inverso son: " << D << " " << C << " " << B << " " << A << "\n";
+    return 0;
+}
